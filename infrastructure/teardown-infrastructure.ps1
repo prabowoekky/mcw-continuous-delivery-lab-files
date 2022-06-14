89ecca7d3a35dd645e8905da370837ff884b63b6
prabowoekky <prabowo.eki@gmail.com>
2022-06-14 01:37:22 +0000

    update

--- infrastructure/teardown-infrastructure.ps1
+++ infrastructure/teardown-infrastructure.ps1
@@ -2,5 +2,7 @@ $studentprefix ="eky"
 $resourcegroupName = "fabmedical-rg-" + $studentprefix
 
 az ad sp delete --id "fabmedical-$studentprefix"
+az ad sp delete --id "fabmedical-eky"
+az ad sp delete --id "BTPN-DI-POC-Fabrikam_Ekky-80d34f0d-a59e-49cf-8ee4-b879b5feb622"
 az group delete --name $resourceGroupName
 
